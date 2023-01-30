113dd2126a7089ccc70d82bf8b5d502273a932f5
DigisCoolisCool <119612928+DigisCoolisCool@users.noreply.github.com>
2023-01-30 16:00:44 +0200

    version1.0

---
+++ DigitalYouth2022-main/DigitalYouth-main/New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	  public int coins;
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start() {
+		coins=0;
+		Abilities.doubleJumpEnabled=true;
+	}
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {  
+			
+			Destroy(collided.gameObject);
+			coins++;
+			HUD.Message("Got");
+			HUD.UpdateCoinCount(coins);
+
+		}
+
+		if (collided.gameObject.name == "Powerup1") {  
+			
+			Destroy(collided.gameObject);
+			HUD.Message("You got powerup and can jump higher");
+			Abilities.doubleJumpEnabled=true;
+			
+
+		}
+
+
+
+	}
+}
